0bee753b3c4dd4dd077830f69875c44700147759
Ofoty <wawlikk@mail.ru>
2020-07-20 22:24:48 +0500

    count of steps per frame increased to 7

--- Engine/Game.cpp
+++ Engine/Game.cpp
@@ -50,15 +50,20 @@ Game::Game(MainWindow& wnd)
 
 void Game::Go()
 {
-	gfx.BeginFrame();	
-	UpdateModel();
+	gfx.BeginFrame();
+	float elapsedTime = ft.Mark();
+	while (elapsedTime > 0.0f)
+	{
+		const float dt = std::min(0.0025f, elapsedTime);
+		UpdateModel(dt);
+		elapsedTime -= dt;
+	}
 	ComposeFrame();
 	gfx.EndFrame();
 }
 
-void Game::UpdateModel()
+void Game::UpdateModel(float dt)
 {
-	float dt = ft.Mark();
 	pad.Update(wnd.kbd, dt);
 	pad.CollidingWithWall(walls);
 	ball.Update(dt);
--- Engine/Game.h
+++ Engine/Game.h
@@ -39,7 +39,7 @@ public:
 	void Go();
 private:
 	void ComposeFrame();
-	void UpdateModel();
+	void UpdateModel(float dt);
 	/********************************/
 	/*  User Functions              */
 	/********************************/
